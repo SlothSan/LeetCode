535fdfa05a0e2d6cd4facf0f01a41a2e74af64fa
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-01-25 11:09:57 +0000

    Refactored solution
    handle further test cases and use only 1 loop;

--- JavaScript/Easy/Two Sum/script.js
+++ JavaScript/Easy/Two Sum/script.js
@@ -1,28 +1,54 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-  let mapOfNumbers = {};
-  let twoIndexes = [];
+  let map = new Map();
 
-  //create a map of all nums in nums with there indexes
   for (let i = 0; i < nums.length; i++) {
-    mapOfNumbers[nums[i]] = i;
-  }
+    let goal = target - nums[i];
 
-  //loop through the nums array, look for the target in the numsMap if present push to the array and return.
-  for (let i = 0; i < nums.length; i++) {
-    let currTarget = target - nums[i];
-    if (mapOfNumbers[currTarget] !== null && mapOfNumbers[currTarget] !== i) {
-      twoIndexes.push(i);
-      twoIndexes.push(mapOfNumbers[currTarget]);
-      break;
+    if (map.has(goal)) {
+      return [i, map.get(goal)];
     }
+
+    map.set(nums[i], i);
   }
-
-  return twoIndexes;
 };
-
-console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([2, 5, 5, 11], 10)); // [2, 1]
